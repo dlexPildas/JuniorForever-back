0575352048e9c7e9b46fbcaa2f3ef44dde8541c2
Daniel Lima de Oliveira <daniel.lima@iatec.com>
2020-04-19 01:57:44 -0300

    Add constructor in AuthorRepository

--- JuniorForever.Repository/AuthorRepository.cs
+++ JuniorForever.Repository/AuthorRepository.cs
@@ -4,11 +4,17 @@ using System.Text;
 using System.Threading.Tasks;
 using JuniorForever.Domain.Interfaces;
 using JuniorForever.Domain.Models;
+using JuniorForever.Repository.Data;
 
 namespace JuniorForever.Repository
 {
     class AuthorRepository : Repository, IAuthorRepository
     {
+
+        public AuthorRepository(DataContext dataContext) : base(dataContext)
+        {
+        }
+
         public Task<Author[]> GetAllAuthorsAsync()
         {
             throw new NotImplementedException();
